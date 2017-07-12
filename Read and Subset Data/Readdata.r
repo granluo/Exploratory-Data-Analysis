33d7069f89ca362d990dcedbc9c1b7c6d3b94580
Zongran <zxl464@case.edu>
2017-07-12 09:32:26 -0700

    add two lessons

--- Read and Subset Data/Readdata.r
+++ Read and Subset Data/Readdata.r
@@ -1,4 +1,14 @@
+library(ggplot2)
+
 statesInfo <-  read.csv('stateData.csv')
 ?subset
 subset (statesInfo, state.region == 1)
 statesInfo[statesInfo$state.region==1, ]
+
+reddit <- read.csv('reddit.csv')
+str(reddit)
+
+levels(reddit$age.range)
+table(reddit$employment.status)
+
+qplot(data = reddit, x = age.range)
